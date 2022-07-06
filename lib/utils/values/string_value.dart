b7c2d2265a023c6180b8ba6d33b0e08ee8ae37fc
JustNewbieProgrammer <mytpewpew@gmail.com>
2022-07-06 13:08:30 +0900

    re-design & fixed error

--- lib/utils/values/string_value.dart
+++ lib/utils/values/string_value.dart
@@ -7,3 +7,41 @@ class StringValue{
   static final String ringRing = '따르릉';
   static final String folderName = 'folderName';
 }
+
+class DatabaseString{
+  static final String tableName = 'alarm';
+  static final String weekRepeatTableName = 'week_repeat';
+  static final String musicPathTableName = 'music_path';
+  static final String alarmFolderTableName = 'alarm_folder';
+  static final String dayOffTableName = 'day_off';
+  static final String columnId = 'id';
+  static final String columnAlarmType = 'alarmType';
+  static final String columnTitle = 'title';
+  static final String columnAlarmDateTime = 'alarmDateTime';
+  static final String columnEndDay = 'endDay';
+  static final String columnAlarmState = 'alarmState';
+  static final String columnAlarmOrder = 'alarmOrder';
+  static final String columnFolderName = 'folderName';
+  static final String columnAlarmInterval = 'alarmInterval';
+  static final String columnMonthRepeatDay = 'monthRepeatDay';
+  static final String columnMusicBool = 'musicBool';
+  static final String columnMusicPath = 'musicPath';
+  static final String columnMusicVolume = 'musicVolume';
+  static final String columnVibrationBool = 'vibrationBool';
+  static final String columnVibrationName = 'vibrationName';
+  static final String columnRepeatBool = 'repeatBool';
+  static final String columnRepeatInterval = 'repeatInterval';
+  static final String columnRepeatNum = 'repeatNum';
+  static final String columnDayOffDate = 'dayOffDate';
+  static final String columnPath = 'path';
+}
+
+class DayOfWeekString{
+  static final String columnSunday = 'sunday';
+  static final String columnMonday = 'monday';
+  static final String columnTuesday = 'tuesday';
+  static final String columnWednesday = 'wednesday';
+  static final String columnThursday = 'thursday';
+  static final String columnFriday = 'friday';
+  static final String columnSaturday = 'saturday';
+}
